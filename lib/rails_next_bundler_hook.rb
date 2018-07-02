3d144f353e4252cf8a651fc64ba7e480428ffef5
Guillaume Lenoir <lenoirguillaume@gmail.com>
2018-07-02 12:10:13 +1200

    asda

--- lib/rails_next_bundler_hook.rb
+++ lib/rails_next_bundler_hook.rb
@@ -1,16 +1,21 @@
 require "rails_next_bundler_hook/version"
 
 module RailsNextBundlerHook
-  if ENV['RAILS_NEXT']
-    module Bundler::SharedHelpers
-      def default_lockfile
-        gemfile = default_gemfile
+  Bundler::Plugin.add_hook('before-install-all') do |dependencies|
+    # fake hook plugin, we do the job below by loading a monkey patch
+    # on plugin load
+  end
+end
 
-        case gemfile.basename.to_s
-        when "gems.rb" then Pathname.new(gemfile.sub(/.rb$/, ".locked"))
-        else Pathname.new("#{gemfile}_rails_next.lock")
-        end.untaint
-      end
+if ENV['RAILS_NEXT']
+  module Bundler::SharedHelpers
+    def default_lockfile
+      gemfile = default_gemfile
+
+      case gemfile.basename.to_s
+      when "gems.rb" then Pathname.new(gemfile.sub(/.rb$/, ".locked"))
+      else Pathname.new("#{gemfile}_rails_next.lock")
+      end.untaint
     end
   end
 end
